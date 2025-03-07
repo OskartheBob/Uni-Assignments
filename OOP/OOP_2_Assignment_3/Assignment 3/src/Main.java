57bee7ed1fb06b5f9e74cb59946929cfc998076c
OskartheBob <oskarsokol.1@gmail.com>
2025-03-07 16:01:35 +0000

    Finished uncommented

--- OOP/OOP_2_Assignment_3/Assignment 3/src/Main.java
+++ OOP/OOP_2_Assignment_3/Assignment 3/src/Main.java
@@ -3,44 +3,84 @@ public class Main {
     public static void main(String[] args){
         Palindrome palindrome = new Palindrome();
 
+        palindrome.operationCount = 0;
         for(int i = 0; i < 1000000; i++){
             boolean decimal = palindrome.reverseComparison(String.valueOf(i));
             boolean binary = palindrome.reverseComparison(palindrome.decimalBinary(i));
             if(decimal && binary){
                 palindrome.reverseCount ++;
             }
+            if(i%50000 == 0 && i != 0){
+                System.out.println("Range to " + i);
+                System.out.print("Time Taken: " + palindrome.reverseTime + "ms | ");
+                System.out.print("Number of Dual Base Palindromes:  " + palindrome.reverseCount + "| ");
+                System.out.println("Number of Operations: " + palindrome.operationCount + "| ");
+            }
         }
-        System.out.println(palindrome.reverseTime);
-        System.out.println(palindrome.reverseCount);
+        System.out.println("Range to 1000000");
+        System.out.print("Time Taken: " + palindrome.reverseTime + "ms | ");
+        System.out.print("Number of Dual Base Palindromes:  " + palindrome.reverseCount + "| ");
+        System.out.println("Number of Operations: " + palindrome.operationCount + "| ");
+        System.out.println("------------------------------------------------------------------------------------------");
 
+        palindrome.operationCount = 0;
         for(int i = 0; i < 1000000; i++){
             boolean decimal = palindrome.elementComparison(String.valueOf(i));
             boolean binary = palindrome.elementComparison(palindrome.decimalBinary(i));
             if(decimal && binary){
                 palindrome.elementCount ++;
             }
+            if(i%50000 == 0 && i != 0){
+                System.out.println("Range to " + i);
+                System.out.print("Time Taken: " + palindrome.elementTime + "ms | ");
+                System.out.print("Number of Dual Base Palindromes:  " + palindrome.elementCount + "| ");
+                System.out.println("Number of Operations: " + palindrome.operationCount + "| ");
+            }
         }
-        System.out.println(palindrome.elementTime);
-        System.out.println(palindrome.elementCount);
+        System.out.println("Range to 1000000");
+        System.out.print("Time Taken: " + palindrome.elementTime + "ms | ");
+        System.out.print("Number of Dual Base Palindromes:  " + palindrome.elementCount + "| ");
+        System.out.println("Number of Operations: " + palindrome.operationCount + "| ");
+        System.out.println("------------------------------------------------------------------------------------------");
 
+        palindrome.operationCount = 0;
         for(int i = 0; i < 1000000; i++){
             boolean decimal = palindrome.stackComparison(String.valueOf(i));
             boolean binary = palindrome.stackComparison(palindrome.decimalBinary(i));
             if(decimal && binary){
                 palindrome.stackCount ++;
             }
+            if(i%50000 == 0 && i != 0){
+                System.out.println("Range to " + i);
+                System.out.print("Time Taken: " + palindrome.stackTime + "ms | ");
+                System.out.print("Number of Dual Base Palindromes:  " + palindrome.stackCount + "| ");
+                System.out.println("Number of Operations: " + palindrome.operationCount + "| ");
+            }
         }
-        System.out.println(palindrome.stackTime);
-        System.out.println(palindrome.stackCount);
+        System.out.println("Range to 1000000");
+        System.out.print("Time Taken: " + palindrome.stackTime + "ms | ");
+        System.out.print("Number of Dual Base Palindromes:  " + palindrome.stackCount + "| ");
+        System.out.println("Number of Operations: " + palindrome.operationCount + "| ");
+        System.out.println("------------------------------------------------------------------------------------------");
 
+        palindrome.operationCount = 0;
         for(int i = 0; i < 1000000; i++){
             boolean decimal = palindrome.recursionComparison(String.valueOf(i));
             boolean binary = palindrome.recursionComparison(palindrome.decimalBinary(i));
             if(decimal && binary){
                 palindrome.recursionCount ++;
             }
+            if(i%50000 == 0 && i != 0){
+                System.out.println("Range to " + i);
+                System.out.print("Time Taken: " + palindrome.recursionTime + "ms | ");
+                System.out.print("Number of Dual Base Palindromes:  " + palindrome.recursionCount + "| ");
+                System.out.println("Number of Operations: " + palindrome.operationCount + "| ");
+            }
         }
-        System.out.println(palindrome.recursionTime);
-        System.out.println(palindrome.recursionCount);
+        System.out.println("Range to 1000000");
+        System.out.print("Time Taken: " + palindrome.recursionTime + "ms | ");
+        System.out.print("Number of Dual Base Palindromes:  " + palindrome.recursionCount + "| ");
+        System.out.println("Number of Operations: " + palindrome.operationCount + "| ");
+        System.out.println("------------------------------------------------------------------------------------------");
     }
 }
--- OOP/OOP_2_Assignment_3/Assignment 3/src/Palindrome.java
+++ OOP/OOP_2_Assignment_3/Assignment 3/src/Palindrome.java
@@ -1,5 +1,5 @@
 public class Palindrome {
-    int reverseOpCnt = 0;
+    int operationCount = 0;
     int reverseTime = 0;
     int elementTime = 0;
     int stackTime = 0;
@@ -15,62 +15,94 @@ public class Palindrome {
 
     public String decimalBinary(int decimal){
         String B_Number = Integer.toBinaryString(decimal);
+        operationCount ++;
         return B_Number;
     }
 
     public boolean reverseComparison(String input){
         long time = System.currentTimeMillis();
+        operationCount += 2;
         String reverse = new StringBuilder(input).reverse().toString();
+        operationCount += 3;
         reverseTime += System.currentTimeMillis() - time;
+        operationCount += 2;
         return reverse.equals(input);
     }
 
     public boolean elementComparison(String input){
         long time = System.currentTimeMillis();
+        operationCount += 2;
         boolean check = true;
+        operationCount ++;
+        operationCount++;
         for(int i = 0; i < input.length(); i++){
+            operationCount += 2;
             char first = input.charAt(i);
+            operationCount += 2;
             char last = input.charAt(input.length() - (1+i));
+            operationCount += 4;
+            operationCount ++;
             if(first != last){
                 check = false;
+                operationCount ++;
                 break;
             }
         }
         elementTime += System.currentTimeMillis() - time;
+        operationCount += 2;
         return check;
     }
 
     public boolean stackComparison(String input){
         long time = System.currentTimeMillis();
+        operationCount += 2;
         boolean check = true;
+        operationCount ++;
         ArrayStack stack = new ArrayStack();
+        operationCount ++;
         ArrayQueue queue = new ArrayQueue();
+        operationCount ++;
+        operationCount ++;
         for(int i = 0; i < input.length(); i++){
+            operationCount += 2;
             char character = input.charAt(i);
+            operationCount += 2;
             stack.push(character);
+            operationCount += 3;
             queue.enqueue(character);
+            operationCount += 3;
         }
+        operationCount ++;
         for(int i = 0; i < input.length(); i++){
+            operationCount += 2;
+            operationCount += 7;
             if(stack.pop() != queue.dequeue()){
                 check = false;
+                operationCount ++;
                 break;
             }
         }
         stackTime += System.currentTimeMillis() - time;
+        operationCount += 2;
         return check;
     }
 
     public String reverse(String input){
+        operationCount += 3;
         if(input == null || input.length() <= 1){
             return input;
         }
+        operationCount ++;
         return reverse(input.substring(1)) + input.charAt(0);
     }
 
     public boolean recursionComparison(String input){
         long time = System.currentTimeMillis();
+        operationCount += 2;
         String recursion = reverse(input);
+        operationCount ++;
         recursionTime += System.currentTimeMillis() - time;
+        operationCount += 2;
         return recursion.equals(input);
     }
 }
