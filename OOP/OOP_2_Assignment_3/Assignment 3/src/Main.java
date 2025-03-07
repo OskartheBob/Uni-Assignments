394b73f352857d826924bf9c16b21bee887f2a89
OskartheBob <oskarsokol.1@gmail.com>
2025-03-07 09:57:28 +0000

    Palindrome class

--- OOP/OOP_2_Assignment_3/Assignment 3/src/Main.java
+++ OOP/OOP_2_Assignment_3/Assignment 3/src/Main.java
@@ -2,6 +2,45 @@ public class Main {
 
     public static void main(String[] args){
         Palindrome palindrome = new Palindrome();
-        palindrome.decimalBinary(1000000);
+
+        for(int i = 0; i < 1000000; i++){
+            boolean decimal = palindrome.reverseComparison(String.valueOf(i));
+            boolean binary = palindrome.reverseComparison(palindrome.decimalBinary(i));
+            if(decimal && binary){
+                palindrome.reverseCount ++;
+            }
+        }
+        System.out.println(palindrome.reverseTime);
+        System.out.println(palindrome.reverseCount);
+
+        for(int i = 0; i < 1000000; i++){
+            boolean decimal = palindrome.elementComparison(String.valueOf(i));
+            boolean binary = palindrome.elementComparison(palindrome.decimalBinary(i));
+            if(decimal && binary){
+                palindrome.elementCount ++;
+            }
+        }
+        System.out.println(palindrome.elementTime);
+        System.out.println(palindrome.elementCount);
+
+        for(int i = 0; i < 1000000; i++){
+            boolean decimal = palindrome.stackComparison(String.valueOf(i));
+            boolean binary = palindrome.stackComparison(palindrome.decimalBinary(i));
+            if(decimal && binary){
+                palindrome.stackCount ++;
+            }
+        }
+        System.out.println(palindrome.stackTime);
+        System.out.println(palindrome.stackCount);
+
+        for(int i = 0; i < 1000000; i++){
+            boolean decimal = palindrome.recursionComparison(String.valueOf(i));
+            boolean binary = palindrome.recursionComparison(palindrome.decimalBinary(i));
+            if(decimal && binary){
+                palindrome.recursionCount ++;
+            }
+        }
+        System.out.println(palindrome.recursionTime);
+        System.out.println(palindrome.recursionCount);
     }
 }
--- OOP/OOP_2_Assignment_3/Assignment 3/src/Palindrome.java
+++ OOP/OOP_2_Assignment_3/Assignment 3/src/Palindrome.java
@@ -1,39 +1,76 @@
 public class Palindrome {
+    int reverseOpCnt = 0;
+    int reverseTime = 0;
+    int elementTime = 0;
+    int stackTime = 0;
+    int recursionTime = 0;
+    int reverseCount = 0;
+    int elementCount = 0;
+    int stackCount = 0;
+    int recursionCount = 0;
 
     public Palindrome(){
 
     }
 
-    public long decimalBinary(int decimal){
-        
-        long B_Number = 0;
-        long cnt = 0;
-        while (decimal != 0) {
-            int rem = decimal % 2;
-            double c = Math.pow(10, cnt);
-            B_Number += rem * c;
-            decimal /= 2;
-
-            // Count used to store exponent value
-            cnt++;
-        }
-        System.out.println(B_Number);
+    public String decimalBinary(int decimal){
+        String B_Number = Integer.toBinaryString(decimal);
         return B_Number;
     }
 
-    /*public boolean reverseComparison(String input){
-
+    public boolean reverseComparison(String input){
+        long time = System.currentTimeMillis();
+        String reverse = new StringBuilder(input).reverse().toString();
+        reverseTime += System.currentTimeMillis() - time;
+        return reverse.equals(input);
     }
 
     public boolean elementComparison(String input){
-
+        long time = System.currentTimeMillis();
+        boolean check = true;
+        for(int i = 0; i < input.length(); i++){
+            char first = input.charAt(i);
+            char last = input.charAt(input.length() - (1+i));
+            if(first != last){
+                check = false;
+                break;
+            }
+        }
+        elementTime += System.currentTimeMillis() - time;
+        return check;
     }
 
     public boolean stackComparison(String input){
+        long time = System.currentTimeMillis();
+        boolean check = true;
+        ArrayStack stack = new ArrayStack();
+        ArrayQueue queue = new ArrayQueue();
+        for(int i = 0; i < input.length(); i++){
+            char character = input.charAt(i);
+            stack.push(character);
+            queue.enqueue(character);
+        }
+        for(int i = 0; i < input.length(); i++){
+            if(stack.pop() != queue.dequeue()){
+                check = false;
+                break;
+            }
+        }
+        stackTime += System.currentTimeMillis() - time;
+        return check;
+    }
 
+    public String reverse(String input){
+        if(input == null || input.length() <= 1){
+            return input;
+        }
+        return reverse(input.substring(1)) + input.charAt(0);
     }
 
     public boolean recursionComparison(String input){
-
-    }*/
+        long time = System.currentTimeMillis();
+        String recursion = reverse(input);
+        recursionTime += System.currentTimeMillis() - time;
+        return recursion.equals(input);
+    }
 }
